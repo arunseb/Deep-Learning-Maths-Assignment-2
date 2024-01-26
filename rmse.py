1ea851a1487bc4ba7e83aff45c2433d72bbb0cf1
Arun Sebastian <91450545+arunseb@users.noreply.github.com>
2024-01-25 21:26:30 -0500

    Add files via upload

---
+++ rmse.py
@@ -0,0 +1,7 @@
+import numpy as np
+def rmse(predictions, targets):
+    pred = np.array(predictions)
+    tar = np.array(targets)
+    mse = np.mean((pred - tar)**2)
+    rmse = np.sqrt(mse) # TODO: Implement RMSE Calculation here...
+    return rmse
